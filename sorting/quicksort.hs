5bc8d09221f1d80ab808fbd273789a3a33504990
krocki <kmrocki@us.ibm.com>
2016-12-16 20:00:15 -0800

    quicksort.hs

--- sorting/quicksort.hs
+++ sorting/quicksort.hs
@@ -1,4 +1,27 @@
 -- @Author: krocki
 -- @Date:   2016-12-16 18:29:38
 -- @Last Modified by:   krocki
--- @Last Modified time: 2016-12-16 18:29:52
+-- @Last Modified time: 2016-12-16 19:59:02
+
+import System.Random
+import Data.List
+
+main = do
+    seed <- newStdGen
+    let unsorted = randomList (100, 1, 1000) seed
+        sorted = quicksort unsorted
+    putStrLn $ "Before sorting: \n" ++ show unsorted
+    putStrLn $ "After sorting: \n" ++ show sorted
+
+randomList :: (Int, Int, Int) -> StdGen -> [Int]
+randomList (n, min, max) gen = take n (randomRs (min,max) gen)
+
+-- let's choose 1st element to be the pivot (x)
+-- we put this pivot in the middle, all elements < x to the left,
+-- all elements >=x to the right and process left and right
+-- recursively until []
+
+quicksort [] = []
+quicksort (x:xs) = quicksort left  ++ [x] ++ quicksort right
+                   where left = [e | e <- xs, e < x]
+                         right = [e | e <- xs, e >= x]
