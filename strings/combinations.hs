fa715d50d8ecee243a8cf1b7729c4256bdbc9523
krocki <kmrocki@us.ibm.com>
2016-12-23 20:13:02 -0800

    combinations.hs

---
+++ strings/combinations.hs
@@ -0,0 +1,16 @@
+-- @Author: krocki
+-- @Date:   2016-12-23 19:20:32
+-- @Last Modified by:   krocki
+-- @Last Modified time: 2016-12-23 20:12:31
+
+import Data.List
+
+-- xs is a sorted list
+combinations' :: (Ord a) => [a] -> Int -> [[a]]
+combinations' [] _ = [[]]
+combinations' _ 0 = [[]]
+combinations' xs elems = [xs !! i:rest | i <- [0..(length xs) - elems], 
+                          rest <- combinations (drop (i+1) xs) (elems-1)]
+
+combinations :: (Ord a) => [a] -> Int -> [[a]]
+combinations xs elems = combinations' (nub (sort xs)) elems
