773d65e1ea8512fe27c0ee78cb07131df345eb23
krocki <kmrocki@us.ibm.com>
2016-12-23 20:13:57 -0800

    combinations.hs

--- strings/combinations.hs
+++ strings/combinations.hs
@@ -1,7 +1,7 @@
 -- @Author: krocki
 -- @Date:   2016-12-23 19:20:32
 -- @Last Modified by:   krocki
--- @Last Modified time: 2016-12-23 20:12:31
+-- @Last Modified time: 2016-12-23 20:13:52
 
 import Data.List
 
@@ -12,5 +12,6 @@ combinations' _ 0 = [[]]
 combinations' xs elems = [xs !! i:rest | i <- [0..(length xs) - elems], 
                           rest <- combinations (drop (i+1) xs) (elems-1)]
 
+-- first sort and remove duplicates
 combinations :: (Ord a) => [a] -> Int -> [[a]]
 combinations xs elems = combinations' (nub (sort xs)) elems
