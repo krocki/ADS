1b455312eb7fc5faa4c69b90cb6a5d841e54234a
krocki <kmrocki@us.ibm.com>
2016-12-31 12:20:46 -0800

    abs

---
+++ bitwise/abs.c
@@ -0,0 +1,35 @@
+/*
+* @Author: krocki
+* @Date:   2016-12-30 22:25:25
+* @Last Modified by:   krocki
+* @Last Modified time: 2016-12-31 12:19:39
+*/
+
+#include <stdio.h>
+
+int absolute(int n) {
+
+	int const mask = n >> (31);
+	return ((n ^ mask) - mask);
+
+	// for positive, do nothing
+	// x =        0b00000000000000000000000000000100
+	// mask =     0b00000000000000000000000000000000
+	// x ^ mask = 0b00000000000000000000000000000100
+	// - mask =   0b00000000000000000000000000000100
+
+	// for negative, toggle all bits and add 1
+	// y =        0b11111111111111111111111111111100
+	// mask =     0b11111111111111111111111111111111 = -1
+	// y ^ mask = 0b00000000000000000000000000000011 = 3
+	// - mask =   0b00000000000000000000000000000100 = + 1
+}
+
+int main() {
+
+	int x = 0b00000000000000000000000000000100;
+	int y = 0b11111111111111111111111111111100;
+
+	printf("x = %d, y = %d abs(x) = %d, abs(y) = %d\n", x, y, absolute(x), absolute(y));
+
+}
