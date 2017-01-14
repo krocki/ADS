a71ae4952a324edc3423116bc7e6aeead86ec1da
krocki <kmrocki@us.ibm.com>
2017-01-14 12:45:38 -0800

    jugs

---
+++ cci/06_05_jugs.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: krocki
+# @Date:   2017-01-14 12:40:02
+# @Last Modified by:   krocki
+# @Last Modified time: 2017-01-14 12:44:36
+
+# you have (a) 5-quart jug, a (b)3-quart jug, and an unlimited supply of water. 
+# How would you come up with exactly four quarts of water?
+
+# fill a, pour 3 into b, a has 2 left, empty b and pour a into b
+# fill a, fill b, now a has 4 quarts
