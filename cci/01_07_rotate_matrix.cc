ed60ef3cc35125e425be5b45f3b7608fb2bd08b4
krocki <kmrocki@us.ibm.com>
2017-01-16 12:19:05 -0800

    rotate.cc

--- cci/01_07_rotate_matrix.cc
+++ cci/01_07_rotate_matrix.cc
@@ -11,18 +11,10 @@
 // i j k l  ->  o k g c
 // m n o p      p l h d
 
-// a b c d      a e i m         m i e a
-// e f g h  T   b f j n flip H  n j f b
-// i j k l  ->  c g k o ->  --> o k g c
-// m n o p      d h l p         p l h d
-
-// in place -> swap a,m, swap a, p, swap a, d
-// repeat for every element
-
 #include <stdlib.h>
 #include <iostream>
 
-void print_array(int arr[][4], int n, int k) {
+void print_array(int arr[][5], int n, int k) {
 
 	for (int i = 0; i < n; i++) {
 
@@ -34,7 +26,12 @@ void print_array(int arr[][4], int n, int k) {
 
 }
 
-void rotate_array(int arr[][4], int n, int k) {
+// a b c d      a e i m         m i e a
+// e f g h  T   b f j n flip H  n j f b
+// i j k l  ->  c g k o ->  --> o k g c
+// m n o p      d h l p         p l h d
+
+void rotate_array(int arr[][5], int n, int k) {
 
 	//transpose
 	for (int i = 0; i < n; i++) {
@@ -54,18 +51,35 @@ void rotate_array(int arr[][4], int n, int k) {
 
 	}
 
-	printf("\n");
-	print_array(arr, 4, 4);
+}
+
+// solution with rotating layers
+void rotate_array_v2(int arr[][5], int n, int k) {
+
+	for (int i = 0; i < n / 2; i++) {
+
+		for (int j = 0; j < n / 2 + 1; j++) {
+
+			int temp = arr[i][j];
+			arr[i][j] = arr[n - 1 - j][i];
+			arr[n - 1 - j][i] = arr[n - 1 - i][n - 1 - j];
+			arr[n - 1 - i][n - 1 - j] = arr[j][n - 1 - i];
+			arr[j][n - 1 - i] = temp;
+		}
+	}
+
 
 }
 
 int main() {
 
-	int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+	int arr[5][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
 
-	print_array(arr, 4, 4);
-	rotate_array(arr, 4, 4);
-
+	print_array(arr, 5, 5);
+	printf("\n");
+	rotate_array_v2(arr, 5, 5);
+	print_array(arr, 5, 5);
+	printf("\n");
 	return 0;
 
 }
