ef08c7757efd5da361472a010a41d02328a70f24
krocki <kmrocki@us.ibm.com>
2017-01-18 11:54:12 -0800

    reverse

---
+++ leetcode/344_reverse_string.cc
@@ -0,0 +1,15 @@
+// 344. Reverse String
+
+class Solution {
+public:
+	string reverseString(string s) {
+
+		for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+
+			swap(s[i], s[j]);
+
+		}
+
+		return s;
+	}
+};
