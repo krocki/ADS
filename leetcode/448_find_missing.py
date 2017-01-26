4d69d82b2700a86487a368d59ec55c34cbe968a0
Kamil M Rocki <kmrocki@us.ibm.com>
2017-01-25 18:22:11 -0800

    missing nums

---
+++ leetcode/448_find_missing.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author: Kamil M Rocki
+# @Date:   2017-01-25 18:08:05
+# @Last Modified by:   Kamil M Rocki
+# @Last Modified time: 2017-01-25 18:21:52
+
+def findDisappearedNumbers(nums):
+	"""
+	:type nums: List[int]
+	:rtype: List[int]
+	"""
+	missing = []
+
+	for i,x in enumerate(nums):
+		idx = abs(x)
+		nums[idx-1] = abs(nums[idx-1]) * (-1)
+
+	print nums
+
+	for i,x in enumerate(nums):
+		if x > 0: missing.append(i+1)
+
+	return missing
+
+print findDisappearedNumbers([4,3,2,7,8,2,3,1])
